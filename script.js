374c2debe16e93468f9a925f753a46394f80c5bf
Samanta <samantadossantos@outlook.com.br>
2022-07-01 13:15:27 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
 const semaforo = document.querySelector("#semaforo_img");
-const botao = document.querySelector('#botao')
+const botao = document.querySelector("#botao");
+let sinal = 0;
 
-const cores = ["vermelho", "amarelo", "verde"];
+let carro = document.querySelector(".carro_img");
 
-
-const ligar = {
-    'vermelho': () => semaforo.scr = './images/vermelho.png',
-    'amarelo': () => semaforo.scr = './images/amarelo.png',
-    "verde": () => semaforo.scr = './images/verde.png'
+function mudarSinal() {
+  if (sinal == 0) {
+    semaforo.src = "./images/vermelho.png";
+    setTimeout(() => {
+      sinal = 1;
+    }, 1000);
+  } else if (sinal == 1) {
+    semaforo.src = "./images/amarelo.png";
+    setTimeout(() => {
+      sinal = 2;
+    }, 1000);
+  } else if (sinal == 2) {
+    semaforo.src = "./images/verde.png";
+    setTimeout(() => {
+      sinal = 3;
+      carroAtravessa();
+    }, 1000);
+  } else if (sinal == 3) {
+    setTimeout(() => {
+      semaforo.src = "./images/vermelho.png";
+      sinal = 1;
+      carroEspera();
+    }, 1000);
   }
+}
 
+function carroAtravessa() {
+  carro.classList.remove("carro_img");
+  carro.classList.add("carro_atravessa");
+}
 
+function carroEspera() {
+  carro.classList.remove("carro_atravessa");
+  carro.classList.add("carro_img");
+}
 
+function automatico() {
+  setInterval(mudarSinal, 1000);
+}
 
-botao.addEventListener('click', mudarSinal)
+botao.addEventListener("click", automatico);
